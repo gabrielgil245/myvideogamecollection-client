89145be4cac962946392b2ba608796b51a4e2701
gabrielgil245 <gabrielgil245@gmail.com>
2021-10-20 20:59:52 -0500

    project setup with bootstrap framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { LandingComponent } from './page/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UtilService } from './service/util.service';
+import { UserService } from './service/user.service';
+import { PlatformService } from './service/platform.service';
+import { GameService } from './service/game.service';
+import { LandingComponent } from './page/landing/landing.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UtilService, UserService, PlatformService, GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
