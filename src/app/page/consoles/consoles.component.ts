aa436c77280407c6b0f4540e5271e8e9f83143ee
gabrielgil245 <gabrielgil245@gmail.com>
2021-10-27 22:35:52 -0500

    user can now delete a game at a time

--- src/app/page/consoles/consoles.component.ts
+++ src/app/page/consoles/consoles.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { GameService } from 'src/app/service/game.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-consoles',
@@ -13,29 +14,45 @@ export class ConsolesComponent implements OnInit, OnDestroy {
   _platformId: number = 0;
   _games: any;
   _selectedGame: number = 0;
+  _isEditting: Boolean = false;
   _observer: Subscription = new Subscription;
 
-  constructor(private route: ActivatedRoute, private gameService: GameService) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private gameService: GameService, private router: Router) { }
 
   ngOnInit(): void {
-    this._platformId = this.route.snapshot.params.platformId;
-    console.log(this._platformId);
-    this._observer = this._games = this.gameService.getUserGames(this._platformId).subscribe(game => {
-      this._games = game.data;
-      console.log(this._games);
-    });
+    this.userService.checkSession().subscribe(user => {
+      if (!user.success) {
+        this.router.navigate(['']);
+      } else if (user.success) {
+        this.loadGames();
+      }
+    })
   }
 
   ngOnDestroy(): void {
     this._observer.unsubscribe();
   }
 
+  loadGames() {
+    this._platformId = this.route.snapshot.params.platformId;
+    console.log(this._platformId);
+    this._observer = this._games = this.gameService.getPlatformGames(this._platformId).subscribe(game => {
+      this._games = game.data;
+    });
+  }
+
   editGame(gameId: number) {
     console.log(gameId);
+    this._isEditting = !this._isEditting;
   }
 
   deleteGame(gameId: number) {
-    console.log(gameId);
+    this._isEditting = false;
+    this.gameService.deleteGame(gameId).subscribe(data => {
+      if(data.success) {
+        this.loadGames();
+      }
+    });
   }
 
 }
--- src/app/service/game.service.ts
+++ src/app/service/game.service.ts
@@ -9,7 +9,11 @@ export class GameService {
 
   constructor(private httpClient: HttpClient, private utilService: UtilService) { }
 
-  getUserGames(platformId: number) {
+  getPlatformGames(platformId: number) {
     return this.httpClient.get<any>(`${this.utilService.getServerDomain()}/myvideogamecollection/api/game/platform/${platformId}`, { withCredentials: true });
+  }
+
+  deleteGame(gameId: number) {
+    return this.httpClient.delete<any>(`${this.utilService.getServerDomain()}/myvideogamecollection/api/game/${gameId}`, { withCredentials: true });
   }
 }
