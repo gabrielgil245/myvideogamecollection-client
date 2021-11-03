fc81fde2a89f23465453efc82ca4812c279729d8
gabrielgil245 <gabrielgil245@gmail.com>
2021-11-02 21:57:23 -0500

    if the user decides not to edit a game, then the values will reset

--- src/app/component/game/game.component.ts
+++ src/app/component/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { GameService } from 'src/app/service/game.service';
 import { UserService } from 'src/app/service/user.service';
@@ -8,7 +8,7 @@ import { UserService } from 'src/app/service/user.service';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnChanges {
 
   @Input()
   _game: any;
@@ -34,20 +34,28 @@ export class GameComponent implements OnInit {
     })
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this._isEditting == 0) {
+      this._gameName = this._game.gameName;
+      this._gameStatus = this._game.gameStatus;
+    }
+  }
+
   editGame(gameName: string, gameStatus: string) {
-    if(gameName != "" || gameStatus != "") {
+    if(gameName == "" || gameStatus == "") {
+      this._isEmpty = true;
+      this._gameName = this._game.gameName;
+      this._gameStatus = this._game.gameStatus;
+    }else {
       this.gameService.editGame(this._game.gameId, gameName, gameStatus).subscribe(game => {
-        console.log(game.success);
         if (game.success) {
           this._game.gameName = gameName;
           this._game.gameStatus = gameStatus;
-          this._isEditting = 0;
           this._isEdittingChange.emit(this._isEditting);
         }
+        this._isEditting = 0;
       })
     }
-    this._isEditting = 0;
   }
-    
 
 }
